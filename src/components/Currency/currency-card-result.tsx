8e2c7a24c7666ffe5f115c7bbcfd2466d2249414
Andrew Costa <andrewcosta.contato@gmail.com>
2023-11-23 23:09:02 -0300

    feat: component created to show the conversion result

--- src/components/Currency/currency-card-result.tsx
+++ src/components/Currency/currency-card-result.tsx
@@ -1,25 +1,72 @@
 import styled from 'styled-components'
-import { FormBlock } from "./card-form-block"
-import { RadioBlock } from "./card-radio-block"
 import { Button } from './card-button'
-import { useExchangeRates } from '../../hooks/useExchangeRates'
 import arrowLeft from '../../assets/arrow-left.svg'
-import { useState } from 'react'
+import { useExchangeRates } from '../../hooks/useExchangeRates'
+import { taxPercentage } from '../../utils/filter-percentage'
 
-export const CurrencyCardResult = () => {
+interface Props {
+  typePurchase: "dinheiro" | "cartão"
+  total: number
+  onClick: () => void
+}
+
+export const CurrencyCardResult = ({ onClick, typePurchase, total }: Props) => {
+  const { data } = useExchangeRates()
+
+  const dolarExchange = Number(data?.ask).toFixed(2).replace('.', ',')
+  const totalExchange = total.toFixed(2).replace('.', ',')
+  const percentage = taxPercentage(typePurchase)
 
   return (
-    <Container>
-      <Button imagem={arrowLeft} title='Voltar' />
+    <CurrencyCardResultContent>
+      <Button onClick={onClick} selected imagem={arrowLeft} title='Voltar' />
 
-    </Container>
+      <CardResultText>
+        <p>O resultado do cálculo é</p>
+        <p>R${totalExchange}</p>
+      </CardResultText>
+
+      <CardResultResume>
+        <CardResultResumeSpan>Compra no {typePurchase} e taxa de {percentage}</CardResultResumeSpan>
+        <CardResultResumeSpan>Cotação do dólar: $ 1,00 = R$ {dolarExchange}</CardResultResumeSpan>
+      </CardResultResume>
+    </CurrencyCardResultContent>
   )
 }
 
-const Container = styled.div`
+const CurrencyCardResultContent = styled.div`
   display: flex;
   flex-direction: column;
   gap: 32px;
   width: 360px;
+`
+
+const CardResultText = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  & > p:nth-child(1){
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 32px;
+  }
+
+  & > p:nth-child(2){
+    color: var(--green);
+    font-size: 64px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 80px;
+  }
+`
+const CardResultResume = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const CardResultResumeSpan = styled.span`
+  color: var(--grey-medium);
 `
 
